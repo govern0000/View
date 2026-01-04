a6671fdaa1f70b3e098ea45a12c74e613a294646
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-01-04 11:33:18 +0800

    Script

--- Script/Module/Library.cmd
+++ Script/Module/Library.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-call ..\Script\Module\Library View.Frame-0.00.00
 call ..\Script\Module\Library View.Draw-0.00.00
 call ..\Script\Module\Library View.Type-0.00.00
 call ..\Script\Module\Library View.View-0.00.00
+call ..\Script\Module\Library View.Frame-0.00.00
 call ..\Script\Module\Library View.Demo-0.00.00
