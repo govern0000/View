753b5b8a8cca63a1f33f8e607b7168855c9636b2
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-19 22:55:41 +0800

    Revert "Script"
    This reverts commit 3b74fc0c964f03037a00403651082834c0f0b981.

--- Script/Module/Library.cmd
+++ Script/Module/Library.cmd
@@ -1,3 +1,8 @@
 @echo off
 
-call ..\Script\Module\Library Alan-View.Script-0.00.00
+call ..\Script\Module\Library Alan-View.Draw-0.00.00
+call ..\Script\Module\Library Alan-View.Type-0.00.00
+call ..\Script\Module\Library Alan-View.Play-0.00.00
+call ..\Script\Module\Library Alan-View.Frame-0.00.00
+call ..\Script\Module\Library Alan-View.View-0.00.00
+call ..\Script\Module\Library Alan-View.Demo-0.00.00
