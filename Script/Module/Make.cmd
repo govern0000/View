fbaf305dbb9fd733c98064bf3de2004091bce228
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-19 22:33:47 +0800

    Script

--- Script/Module/Make.cmd
+++ Script/Module/Make.cmd
@@ -1,8 +1,3 @@
 @echo off
 
-call ..\Script\Module\Make View View.Draw
-call ..\Script\Module\Make View View.Type
-call ..\Script\Module\Make View View.Play
-call ..\Script\Module\Make View View.Frame
-call ..\Script\Module\Make View View.View
-call ..\Script\Module\Make View View.Demo
+call ..\Script\Module\Make View/Script Module
