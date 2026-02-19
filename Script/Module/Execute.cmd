861b786d8c3f7a662068321f38040447e8e5d2a5
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-19 22:57:31 +0800

    Script

--- Script/Module/Execute.cmd
+++ Script/Module/Execute.cmd
@@ -1,10 +1,3 @@
 @echo off
 
-set ModuleFold=..\Class\Out\Class\Module
-
-set InfraFold=C:\Users\aaabb\Package\Class\Out\Class\Infra
-
-pushd %ModuleFold%
-set "PATH=%InfraFold%;%PATH%" & set "QT_PLUGIN_PATH=%InfraFold%" & ..\Library\View.Demo-0.00.00
-echo Status: %errorlevel%
-popd
+call ..\Script\Module\Execute View.Demo -
