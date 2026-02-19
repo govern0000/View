ac9b0c65f2d5c9580e44e24eea9609b71559a123
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-02-19 21:37:47 +0800

    Script

--- Script/Module/Library.cmd
+++ Script/Module/Library.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-call ..\Script\Module\Library View.Draw-0.00.00
-call ..\Script\Module\Library View.Type-0.00.00
-call ..\Script\Module\Library View.Play-0.00.00
-call ..\Script\Module\Library View.Frame-0.00.00
-call ..\Script\Module\Library View.View-0.00.00
-call ..\Script\Module\Library View.Demo-0.00.00
+call ..\Script\Module\Library Alan-View.Draw-0.00.00
+call ..\Script\Module\Library Alan-View.Type-0.00.00
+call ..\Script\Module\Library Alan-View.Play-0.00.00
+call ..\Script\Module\Library Alan-View.Frame-0.00.00
+call ..\Script\Module\Library Alan-View.View-0.00.00
+call ..\Script\Module\Library Alan-View.Demo-0.00.00
