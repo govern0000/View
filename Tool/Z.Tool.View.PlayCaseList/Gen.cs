12c47cfd4ae4adaab3e7fa036bf139919a42e83e
Alan Ng <aaabbbcccddd00001111@gmail.com>
2026-03-18 14:04:57 +0800

    Tool

--- Tool/Z.Tool.View.PlayCaseList/Gen.cs
+++ Tool/Z.Tool.View.PlayCaseList/Gen.cs
@@ -6,13 +6,13 @@ public class Gen : SourceGen
     {
         base.Init();
         this.Module = this.S("View.Play");
-        this.ClassName = this.S("StatusList");
+        this.ClassName = this.S("CaseList");
         this.BaseClassName = this.S("Any");
         this.AnyClassName = this.S("Any");
-        this.ItemClassName = this.S("Status");
+        this.ItemClassName = this.S("Case");
         this.ArrayClassName = this.S("Array");
         this.Export = true;
-        this.StatItemClassName = this.S("PlayStatus");
+        this.StatItemClassName = this.S("PlayCase");
         this.ItemListFileName = this.GetStatItemListFileName();
         return true;
     }
